420398d1617c5f39fc58e063433136990c68043b
shreyah <shreyah@login.cl.upenn.edu>
2025-05-14 12:23:02 -0400

    politeness results

--- src/emotion.py
+++ src/emotion.py
@@ -5,6 +5,7 @@ import openai
 from openai import OpenAI
 import time
 from tqdm import tqdm
+import json
 
 from prompts.claim_decomposition import decomposition_emotion
 from prompts.relevance_filtering import relevance_emotion
@@ -14,17 +15,84 @@ from prompts.explanations import vanilla_baseline, cot_baseline, socratic_baseli
 from diskcache import Cache
 cache = Cache("/shared_data0/shreyah/llm_cache")
 
+prompt_dict = {"vanilla": vanilla_baseline,
+               "cot": cot_baseline,
+               "socratic": socratic_baseline,
+               "subq": least_to_most_baseline}
+
+emotion_labels = {
+    0: "admiration",
+    1: "amusement",
+    2: "anger",
+    3: "annoyance",
+    4: "approval",
+    5: "caring",
+    6: "confusion",
+    7: "curiosity",
+    8: "desire",
+    9: "disappointment",
+    10: "disapproval",
+    11: "disgust",
+    12: "embarrassment",
+    13: "excitement",
+    14: "fear",
+    15: "gratitude",
+    16: "grief",
+    17: "joy",
+    18: "love",
+    19: "nervousness",
+    20: "optimism",
+    21: "pride",
+    22: "realization",
+    23: "relief",
+    24: "remorse",
+    25: "sadness",
+    26: "surprise",
+    27: "neutral"
+}
+
 class EmotionExample:
     def __init__(self, text, ground_truth, llm_label, llm_explanation):
         self.text = text
         self.ground_truth = ground_truth
         self.llm_label = llm_label
         self.llm_explanation = llm_explanation
+        self.accuracy = 0.0
         self.claims = []
         self.relevant_claims = []
         self.alignment_scores = []
         self.alignment_categories = []
         self.alignment_reasonings = []
+        self.final_alignment_score = 0.0
+
+    def print(self, verbose=False):
+        print("Text: ", self.text)
+        print("Ground Truth: ", self.ground_truth)
+        print("LLM Label: ", self.llm_label)
+        print("LLM Explanation: ", self.llm_explanation)
+        print("Claims: ", self.claims)
+        print("Relevant Claims: ", self.relevant_claims)
+        print("Alignment Scores: ", self.alignment_scores)
+        
+        print("Final Alignment Score: ", self.final_alignment_score)
+        if(verbose):
+            print("Alignment Categories: ", self.alignment_categories)
+            print("Alignment Reasonings: ", self.alignment_reasonings)
+    
+    def to_dict(self):
+        return {
+            'text': self.text,
+            'ground_truth': self.ground_truth,
+            'llm_label': self.llm_label,
+            'llm_explanation': self.llm_explanation,
+            'accuracy': self.accuracy,
+            'claims': self.claims,
+            'relevant_claims': self.relevant_claims,
+            'alignment_scores': self.alignment_scores,
+            'alignment_categories': self.alignment_categories,
+            'alignment_reasonings': self.alignment_reasonings,
+            'final_alignment_score': self.final_alignment_score
+        }
 
 @cache.memoize()
 def query_openai(prompt, model="gpt-4o"):
@@ -49,21 +117,28 @@ def query_openai(prompt, model="gpt-4o"):
             time.sleep(3)
     return "ERROR"
 
-def get_llm_generated_answer(text: str):
-    prompt = emotion_prompt.replace("[BASELINE_PROMPT]", vanilla_baseline).format(text)
+def get_llm_generated_answer(text: str, baseline: str = "vanilla"):
+    prompt = emotion_prompt.replace("[BASELINE_PROMPT", prompt_dict[baseline]).format(text)
     response = query_openai(prompt)
     if response == "ERROR":
         print("Error in querying OpenAI API")
         return None
     response_split = [e for e in response.split("\n") if (e != '' and e.split()[0] in ['Label:', 'Explanation:'])]
-    llm_label = response_split[0].split("Label: ")[1].strip()
+    llm_label = response_split[0].split("Label: ")[1].strip().lower()
     explanation = response_split[1].split("Explanation: ")[1].strip()
-    return llm_label, explanation
+    try:
+        assert(llm_label in emotion_labels.values())
+        assert(len(explanation) > 10)
+        return llm_label, explanation
+    except:
+        print("ERROR: LLM generated answer is not valid")
+        print(response)
+        return None
 
 
 def isolate_individual_features(explanation: str):
     prompt = decomposition_emotion.format(explanation)
-    response = query_openai(prompt)
+    response = query_openai(prompt).replace("\n\n", "\n")
     if response == "ERROR":
         print("Error in querying OpenAI API")
         return None
@@ -73,7 +148,7 @@ def isolate_individual_features(explanation: str):
 
 def is_claim_relevant(text: str, rating: str, claim: str):
     prompt = relevance_emotion.format(text, rating, claim)
-    response = query_openai(prompt)
+    response = query_openai(prompt).replace("\n\n", "\n")
     if response == "ERROR":
         print("Error in querying OpenAI API")
         return None
@@ -81,6 +156,13 @@ def is_claim_relevant(text: str, rating: str, claim: str):
     response = response.split("\n")
     relevance = response[0].strip()
     reasoning = response[1].replace("Reasoning:", "").strip()
+    try:
+        assert(relevance in ["Yes", "No"])
+        assert(len(reasoning) > 10)
+    except:
+        print("ERROR: Could not determine relevance")
+        print(response)
+        return None
     return relevance, reasoning
 
 
@@ -96,7 +178,7 @@ def distill_relevant_features(example: EmotionExample):
 
 def calculate_expert_alignment_score(claim: str):
     prompt = alignment_emotion.format(claim)
-    response = query_openai(prompt)
+    response = query_openai(prompt).replace("\n\n", "\n")
     if response == "ERROR":
         print("Error in querying OpenAI API")
         return None
@@ -105,11 +187,74 @@ def calculate_expert_alignment_score(claim: str):
     response = [r for r in response if r.strip() != ""]
     category = response[0].strip()
     alignment_score = response[1].replace("Category Alignment Rating:", "").strip()
+    reasoning = response[2].replace("Reasoning:", "").strip()
     try:
         alignment_score = float(alignment_score)
+        assert(len(category) > 5)
+        assert(len(reasoning) > 10)
     except:
         print("ERROR: Could not convert alignment score to float")
         print(response)
         alignment_score = 0.0
-    reasoning = response[2].replace("Reasoning:", "").strip()
     return category, alignment_score, reasoning
+
+def load_emotion_data():
+    emotion_data =  load_dataset("BrachioLab/emotion")
+    emotion_data = emotion_data['train'].to_pandas()
+    emotion_data = emotion_data.sample(2, random_state=11).reset_index(drop=True)
+    return emotion_data
+
+
+def run_pipeline(emotion_data, baseline="vanilla"):
+    emotion_examples = []
+    for idx,row in tqdm(emotion_data.iterrows()):
+        label, explanation = get_llm_generated_answer(row['text'], baseline)
+        if label is None:
+            continue
+        emotion_examples.append(EmotionExample(
+            text=row['text'],
+            ground_truth=emotion_labels[int(row['labels'][0])],
+            llm_label=label,
+            llm_explanation=explanation
+        ))
+    
+    for example in emotion_examples:
+        example.accuracy = int(example.ground_truth == example.llm_label)
+
+    for example in emotion_examples:
+        claims = isolate_individual_features(example.llm_explanation)
+        if claims is None:
+            continue
+        example.claims = [claim.strip() for claim in claims]
+
+    for example in emotion_examples:
+        relevant_claims = distill_relevant_features(example)
+        example.relevant_claims = relevant_claims
+
+    for example in emotion_examples:
+        alignment_scores = []
+        alignment_categories = []
+        alignment_reasonings = []
+        for claim in tqdm(example.relevant_claims):
+            category, alignment_score, reasoning = calculate_expert_alignment_score(claim)
+            if category is None:
+                continue
+            alignment_scores.append(alignment_score)
+            alignment_categories.append(category)
+            alignment_reasonings.append(reasoning)
+        example.alignment_scores = alignment_scores
+        example.alignment_categories = alignment_categories
+        example.final_alignment_score = np.sum(alignment_scores)/len(example.claims)
+        example.alignment_reasonings = alignment_reasonings
+
+        
+    data_to_save = [example.to_dict() for example in emotion_examples]
+    with open("../results/{}/emotion_gpt-4o.json".format(baseline), 'w') as f:
+        json.dump(data_to_save, f, indent=4)
+
+if __name__ == "__main__":
+    emotion_data = load_emotion_data()
+    run_pipeline(emotion_data, baseline="vanilla")
+    run_pipeline(emotion_data, baseline="cot")
+    run_pipeline(emotion_data, baseline="socratic")
+    run_pipeline(emotion_data, baseline="subq")
